3966d92a8ca8f1539e68ca9ad9ffb16eb0dca668
Karim Gomez <55936@VNSNY.ORG>
2021-06-17 23:26:13 -0400

    updated game and index html

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 var questions = [
-    {},
-    {},
-    {},
-    {
-
-    }
+    ['What color are apples?', 'Red'],
+    ['what color are bananas?', 'Yellow'],
+    ["What color are the inside of Kiwi's", 'Green'],
+    ["what color are oranges?", "Orange"]
 ];
+
+document.write(questions)
 
 var score = 0;
